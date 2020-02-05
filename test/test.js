38c3696586573f78431ef864406b7eab45417c41
BranceLee <475815059@qq.com>
2020-02-05 11:06:58 +0800

    change test.js

--- test/test.js
+++ test/test.js
@@ -2,6 +2,6 @@ var assert = require('assert');
 
 describe('Test Demo',()=>{
 	it('Find array index', ()=>{
-		assert.equal(-1,[1, 2, 3].indexOf(0))
+		assert.equal(-1,[1, 2, 3].indexOf(1))
 	})
 });
